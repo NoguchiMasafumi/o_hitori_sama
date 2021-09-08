2816fbe621015c9aada555615fd536ae21fb1ab4
NoguchiMasafumi <89947969+NoguchiMasafumi@users.noreply.github.com>
2021-09-08 10:40:43 +0900

    Add files via upload

--- js/vue_control.js
+++ js/vue_control.js
@@ -1,7 +1,7 @@
-var app = new Vue({
-    el: '#body',
+var body = new Vue({
+    el: '#body_bg',
     data: {
-      background-image-url: 'background-image: url("https://cc.jpg");'
+      backgroundimageurl: {"background-image": "url('https://lh3.googleusercontent.com/_e032nu_ZF5VOtmpNL9wTy2mz1KRb4GAlLqb7uuyWguDhiqxtdF3AlqAxtVb_XyLno6zIf34_eaFAn6AyF_IwgMEWQyBvhzb-PYCoVr_lEC53-860wXjvw3YGdspprM1C2NAvYSp2S8=w1500-no?authuser=1')"}
     }
 })
 
